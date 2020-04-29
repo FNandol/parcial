c8fbf7f8f7e84e689f55406de755ebfd2ab0255d
rendonf <43002950+rendonf@users.noreply.github.com>
2020-04-28 22:21:50 -0500

    Update autoevaluacion

--- src/app/pages/autoevaluacion/autoevaluacion.page.ts
+++ src/app/pages/autoevaluacion/autoevaluacion.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-autoevaluacion',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./autoevaluacion.page.scss'],
 })
 export class AutoevaluacionPage implements OnInit {
+  customPickerOptions: any;
 
-  constructor() { }
+  constructor(private alertCtrl: AlertController) {
+    this.customPickerOptions = {
+      buttons: [{
+        text: 'Save',
+        handler: () => console.log('Clicked Save!')
+      }, {
+        text: 'Log',
+        handler: () => {
+          console.log('Clicked Log. Do not Dismiss.');
+          return false;
+        }
+      }]
+    }
+  }
 
   ngOnInit() {
+  }
+
+  async presentConfirm() {
+    const alert = await this.alertCtrl.create({
+      header: 'COVID19 - Ministerio de Salud',
+      message: 'Este formulario tiene carácter de declarión '
+        + 'jurada, hacer una falsa declaración es '
+        + 'considerada una falta grave',
+      buttons: [
+        {
+          text: 'ENVIAR',
+          role: 'CANCELAR',
+          handler: () => {
+            // console.log('Cancel clicked'); aqui se coloca lo que va a hacer el boton al presionarse
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
 }
